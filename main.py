4fb9ca6d0849eefdd59feb2381eb9188bf9df739
Amaury Medeiros <amaurymedeiros@gmail.com>
2015-10-29 18:54:32 -0300

    Use dotenv to hide API_KEY

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import os
 import sys
+
+from dotenv import load_dotenv
 
 from datafetcher import DataFetcher
 
-API_KEY = # Hidden
+DOTENV_PATH = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(DOTENV_PATH)
 
 if len(sys.argv) == 1:
-    actor_name = raw_input("Actor name: ")
+    actor_name = raw_input('Actor name: ')
 else:
     actor_name = ' '.join(sys.argv[1:])
 
-df = DataFetcher(API_KEY, actor_name)
+df = DataFetcher(os.environ.get('API_KEY'), actor_name)
 
 print df.get_actor_movie_parts()
